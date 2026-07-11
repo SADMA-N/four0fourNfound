0238c88dfe0f6315f9f40eaa69d51849f03b25e3
SADMA-N <185231556+SADMA-N@users.noreply.github.com>
2026-07-11 21:37:40 +0600

    feat(backend): configure settings, url routing, and scaffold api app

---
+++ backend/api/urls.py
@@ -0,0 +1,10 @@
+#to not get ModuleNotFoundError
+
+from django.urls import path
+
+# Placeholder routing patterns
+urlpatterns = []
+
+
+
+#In Django, each app has its own  urls.py  module containing a list called  urlpatterns 
